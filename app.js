a1ba2822db5105faf326aff97e2f7b438f4f638c
unknown <vijaylunagariyago@gmail.com>
2020-04-19 15:46:01 +0530

    first admin

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', express.static(path.join(__dirname, 'public/dist')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
