d1bc6ce735c30105f34930bb8186efb562c3fff5
vijaypatel75740 <63951864+vijaypatel75740@users.noreply.github.com>
2020-10-29 10:54:41 +0530

    Update index.js
    watts up update

--- routes/index.js
+++ routes/index.js
@@ -1436,7 +1436,7 @@ let linkRequest1;
     "body": randomMonth + ggff
   }
     request({
-      uri: "https://api.chat-api.com/instance187276/sendMessage?token=w6iiaq8494t3b85j",
+      uri: "https://api.chat-api.com/instance188491/sendMessage?token=t14z9izkagkttw2g",
       method: "POST",
       body: JSON.stringify(linkRequest1),
       headers: requestHeaders1
@@ -1536,7 +1536,7 @@ let linkRequest1;
     "body": randomMonth + ggff
   }
     request({
-      uri: "https://api.chat-api.com/instance187274/sendMessage?token=a0zqmafkpf7gxnxc",
+      uri: "https://api.chat-api.com/instance188490/sendMessage?token=rl7f1y8znyg3du0z",
       method: "POST",
       body: JSON.stringify(linkRequest1),
       headers: requestHeaders1
@@ -1638,7 +1638,7 @@ let linkRequest1;
       "caption": randomMonth + ggff
     }
     request({
-      uri: "https://api.chat-api.com/instance187276/sendFile?token=w6iiaq8494t3b85j",
+      uri: "https://api.chat-api.com/instance188491/sendFile?token=t14z9izkagkttw2g",
       method: "POST",
       body: JSON.stringify(linkRequest1),
       headers: requestHeaders1
@@ -1740,7 +1740,7 @@ let linkRequest1;
       "caption": randomMonth + ggff
     }
     request({
-      uri: "https://api.chat-api.com/instance187274/sendFile?token=a0zqmafkpf7gxnxc",
+      uri: "https://api.chat-api.com/instance188490/sendFile?token=rl7f1y8znyg3du0z",
       method: "POST",
       body: JSON.stringify(linkRequest1),
       headers: requestHeaders1
