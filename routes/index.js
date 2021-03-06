1c9450332d2f947e221c02931b4615516335e194
vijaypatel75740 <63951864+vijaypatel75740@users.noreply.github.com>
2021-03-06 14:16:00 +0530

    Update index.js
    autopost api chane

--- routes/index.js
+++ routes/index.js
@@ -638,6 +638,38 @@ router.post('/api/addAllInOneData', function (req, res) {
   });
 });
 
+router.post('/api/autoPhotoPostFlags', function (req, res) {
+  async.waterfall([
+    function (nextCall) {
+      values =  [
+                   req.body.autopost_flag_tele,
+                   req.body.delay,
+                ]
+      var sqlss = "UPDATE post_flags set autopost_flag_tele =? ,delay =? WHERE id = 1";
+      connection.query(sqlss, values, function (err, rides) {
+        if (err) {
+          return nextCall({
+            "message": "something went wrong",
+          });
+        }
+        nextCall(null, rides[0]);
+      })
+    }
+  ], function (err, response) {
+    if (err) {
+      return res.send({
+        status: err.code ? err.code : 400,
+        message: (err && err.msg) || "someyhing went wrong"
+      });
+    }
+    return res.send({
+      status: 200,
+      message: "Edit post flag update sucessfully",
+      data: response
+    });
+  });
+});
+
 router.post('/api/addtokenData', function (req, res) {
   async.waterfall([
     function (nextCall) {
